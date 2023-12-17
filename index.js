b470a63669191584c42734d5843caaf317f14f93
ChepkemoiS <memoi24sharon@gmail.com>
2023-12-18 02:05:50 +0630

    Completed assignment

---
+++ index.js
@@ -0,0 +1,21 @@
+const divide = function() {
+    return 2000 / 100;
+  };
+  
+  divide(2000 / 100);
+  console.log( divide(2000 / 100));
+
+  const square = x => x * x;
+  let x = 2;
+  let answer = (square(x));
+  console.log(answer);
+
+  const add = (a, b) => {
+    return a + b;
+  };
+  
+  let a = 3;
+  let b = 4;
+  
+  let result = add(a, b);
+  console.log(result); 
